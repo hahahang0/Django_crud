4794da25a3e002160e77c90f389777dc6b1a5f78
hangthimLimbu <hanggoesdsaiml@gmail.com>
2025-02-10 13:33:36 +0545

    CRUD PRACTICE https://www.youtube.com/watch?v=eLZDs07azto (2025/02/10)

---
+++ employee/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path;
+from . import views
+urlpatterns = [
+    path("",views.employee_list,name="list"),
+    path("create/",views.create_employee,name="create"),
+    path("update/<int:pk>",views.update_employee,name="update"),
+    path("delete/<int:pk>",views.delete_employee,name="delete")
+
+]
