d92f61c6d013e0b8f3556cf7fd9ad0822ee1bd2e
hangthimLimbu <hanggoesdsaiml@gmail.com>
2025-02-12 10:48:25 +0545

    some automation changes

--- employee/models.py
+++ employee/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Employee(models.Model):
-    employee_id = models.CharField(max_length=20)
+    # employee_id = models.CharField(max_length=20,primary_key=True)
+    employee_id = models.AutoField(primary_key=True)
     employee_name = models.CharField(max_length=255)
     employee_email = models.EmailField()
     employee_contact = models.CharField(max_length=20)
